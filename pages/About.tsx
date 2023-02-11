68f41af6ffb0200103bef9f9cacaa2256ecd4207
Keez <74885533+sukhraj123@users.noreply.github.com>
2023-02-11 10:58:58 -0800

    Add files via upload

---
+++ pages/About.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/Header'
+import A from '../components/A'
+import Footer from '../components/Footer'
+
+function About() {
+  return (
+    
+    <div id='About'>
+        <Header />
+        <A />
+        <Footer />
+    </div>
+  )
+}
+
+export default About
---
+++ pages/Clients.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Client from '../components/Client'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
+function Clients() {
+  return (
+    <div >
+        <Header />
+        <Client />
+        <Footer />
+    </div>
+  )
+}
+
+export default Clients
